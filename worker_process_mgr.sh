fd25b58333227c00e1a2773552fdfd0262839632
root <root@android01.(none)>
2014-10-25 16:27:57 +0900

    asdfafd

--- worker_process_mgr.sh
+++ worker_process_mgr.sh
@@ -2,15 +2,15 @@
 # step 1 :kill all process
 ps -ef | grep python | grep worker |  awk '{ print $2 }' | sudo xargs kill -9
 # step 2 : run all process with screen
-for i in 2 3 4 5 6 7 8 9 10
+for i in 2 3
 do
    #echo "Welcome $i times"
    #screen -X -S "worker$i" quit
-   if ! screen -list |grep -q "worker$i"; then 
-	screen -S "worker$i" -d -m   	
+   if ! screen -list |grep -q "worker$i"; then
+        screen -S "worker$i" -d -m
    fi
    sleep 0.1
-   screen -S "worker$i" -X stuff "sudo python worker.py worker$i 
+   screen -S "worker$i" -X stuff "sudo python worker.py worker$i
 "
    sleep 0.1
 done
@@ -20,10 +20,9 @@ ps -ef|grep python | grep mon | awk '{print $2}' | sudo xargs kill -9
 
 sleep 0.1
 #if none, make monitor screen
-if ! screen -list |grep -q "monitor"; then
-        screen -S "monitor" -d -m
+if ! screen -list |grep -q "monitor1"; then
+        screen -S "monitor1" -d -m
 fi
 sleep 0.1
-screen -S "monitor" -X stuff "sudo python /home/urqa/worker/urqa_worker/worker_monitor/mon.py
+screen -S "monitor1" -X stuff "sudo python /home/urqa/worker/urqa_worker/worker_monitor/mon.py
 "
-
