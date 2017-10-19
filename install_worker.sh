6c3105588fd7e40d34b21dfe77ee9fc2bc7eab89
Hyun Jong Kim <hyunjongkim@HyunJongKim-MacBook-Pro.local>
2017-10-19 17:41:38 +0900

    arranged

--- install_worker.sh
+++ install_worker.sh
@@ -1,13 +1,13 @@
-git clone https://github.com/UrQA/Worker.git
-mkdir poolfromdjango
-mkdir poolfromdjango/sopool
-mkdir poolfromdjango/sympool
-mkdir poolfromdjango/mappool
-mkdir poolfromworker
-mkdir poolfromworker/logpool
-mkdir poolfromworker/dmppool
-echo "you should set rsync"
-apt-get install python-pip
+sudo git clone https://github.com/UrQA/Worker.git
+sudo mkdir poolfromdjango
+sudo mkdir poolfromdjango/sopool
+sudo mkdir poolfromdjango/sympool
+sudo mkdir poolfromdjango/mappool
+sudo mkdir poolfromworker
+sudo mkdir poolfromworker/logpool
+sudo mkdir poolfromworker/dmppool
+sudo echo "you should set rsync"
+sudo apt-get install python-pip
 sudo apt-get --fix-missing  install openjdk-7-jre
 sudo apt-get install python-jpype
 sudo apt-get install python-sqlalchemy
@@ -16,4 +16,4 @@ sudo pip install pytz
 sudo apt-get install redis-server
 sudo apt-get install python-redis
 sudo /etc/init.d/redis-server restart
-apt-get install python-mysqldb
+sudo apt-get install python-mysqldb
